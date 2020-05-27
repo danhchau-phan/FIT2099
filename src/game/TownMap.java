d57dff047989dbbc0197cfe2bfa3a7115bab8f0e
Nguyen Phan <npha0029@student.monash.edu>
2020-05-27 17:30:53 +0800

    add TownMap class

---
+++ src/game/TownMap.java
@@ -0,0 +1,14 @@
+package game;
+
+import java.util.List;
+
+import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.GroundFactory;
+public class TownMap extends GameMap {
+
+	public TownMap(GroundFactory groundFactory, List<String> lines) {
+		super(groundFactory, lines);
+		// TODO Auto-generated constructor stub
+	}
+
+}
