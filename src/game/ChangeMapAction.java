1fcac4f9ddca15c3d9a7b26f8cc1476589d8f38d
Nguyen Phan <npha0029@student.monash.edu>
2020-05-28 16:42:17 +0800

    allow player to move back and forth between 2 maps

---
+++ src/game/ChangeMapAction.java
@@ -0,0 +1,40 @@
+package game;
+
+import edu.monash.fit2099.engine.Action;
+import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.NumberRange;
+
+public class ChangeMapAction extends Action {
+	private GameMap destination;
+	private GameMap start;
+	
+	public ChangeMapAction(GameMap start, GameMap destination) {
+		this.start = start;
+		this.destination = destination;
+	}
+	@Override
+	public String execute(Actor actor, GameMap map) {
+		
+		NumberRange xRange = destination.getXRange();
+		NumberRange yRange = destination.getYRange();
+		int x, y;
+		do {
+			x = (int) Math.floor(Math.random() * (xRange.max() - xRange.min()+2) + xRange.min());
+			y = (int) Math.floor(Math.random() * (yRange.max() - yRange.min()+2) + yRange.min());
+			System.out.println(x+"     "+y);
+		} while (destination.at(x,y).containsAnActor());
+		map.moveActor(actor, destination.at(x,y));
+		destination.at(x,y).addItem(new Vehicle(destination, start));
+		return menuDescription(actor);
+	}
+
+	@Override
+	public String menuDescription(Actor actor) {
+		if (this.destination instanceof TownMap)
+			return actor + " moves to Town";
+		else
+			return actor + " moves out of Town";
+	}
+	
+}
--- src/game/Vehicle.java
+++ src/game/Vehicle.java
@@ -4,18 +4,21 @@ import java.util.Arrays;
 import java.util.List;
 
 import edu.monash.fit2099.engine.Action;
+import edu.monash.fit2099.engine.GameMap;
 import edu.monash.fit2099.engine.Item;
 
 public class Vehicle extends Item {
-	private TownMap townMap;
+	private GameMap destination;
+	private GameMap start;
 	
-	public Vehicle(TownMap townMap) {
+	public Vehicle(GameMap start, GameMap destination) {
 		super("vehicle", DisplayChar.VEHICLE.toChar(), false);
-		this.townMap = townMap;
+		this.start = start;
+		this.destination = destination;
 	}
 	
 	@Override
 	public List<Action> getAllowableActions() {
-		return Arrays.asList(new MoveToTownAction(townMap));
+		return Arrays.asList(new ChangeMapAction(start, destination));
 	}
 }
