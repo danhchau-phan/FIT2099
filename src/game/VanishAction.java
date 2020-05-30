c006c7ae9ffaff4a39ea1ce20f2f0c381bd32748
Nguyen Phan <npha0029@student.monash.edu>
2020-05-30 14:31:05 +0800

    remove updatePopulation from VanishAction

--- src/game/VanishAction.java
+++ src/game/VanishAction.java
@@ -4,12 +4,11 @@ import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
 
-public class VanishAction extends Action {
+class VanishAction extends Action {
 
 	@Override
 	public String execute(Actor actor, GameMap map) {
 		map.removeActor(actor);
-		actor.updatePopulation();
 		return menuDescription(actor);
 	}
 
