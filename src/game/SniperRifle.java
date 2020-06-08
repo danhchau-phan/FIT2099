a5cae3c198c8d88f84dc2be2ffe5caded8fa23ab
Dilshan Jayasinghe <djay0008@student.monash.edu>
2020-06-08 16:34:08 +0800

    Added SniperRifle class

---
+++ src/game/SniperRifle.java
@@ -0,0 +1,13 @@
+package game;
+
+import edu.monash.fit2099.engine.WeaponItem;
+
+
+public class SniperRifle extends WeaponItem {
+
+    private static final int DAMAGE = 60;
+
+    public SniperRifle() {
+        super("Sniper Rifle", DisplayChar.SNIPER.toChar(), DAMAGE, "SNIPED");
+    }
+}
